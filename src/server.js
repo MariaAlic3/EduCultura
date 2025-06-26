359c99e81d15593c27256704183d48701b85b9f3
Alice-PDA <mariaaliceifba@gmail.com>
2025-06-26 09:46:17 -0300

    :sparkles: feat: add render configuration in server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import universidadesRoutes from './routes/universidades.routes.js';
 import { errorHandler } from './middleware/errorHandler.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware para interpretar JSON
 app.use(express.json());
